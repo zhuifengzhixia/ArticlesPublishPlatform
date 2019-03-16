3cf50d34bc2935ffb4ae1d780b0cf6adca1ee930
weimeng <1746387101@qq.com>
2019-03-16 22:24:35 +0800

    Init project

---
+++ src/main/java/com/example/capp/CappApplication.java
@@ -0,0 +1,13 @@
+package com.example.capp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CappApplication.class, args);
+    }
+
+}
