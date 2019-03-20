3b22d11218543aff3ac33a5e1cf2b61cb54235d6
weimeng <1746387101@qq.com>
2019-03-21 01:41:30 +0800

    update

---
+++ src/main/java/com/example/capp/service/impl/CommentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.capp.service.impl;
+
+import com.example.capp.service.CommentService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentServiceImpl implements CommentService {
+}
