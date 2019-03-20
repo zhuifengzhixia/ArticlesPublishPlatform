7b44dde0eb7855b066dbd5286508d47c6ffe7666
weimeng <1746387101@qq.com>
2019-03-21 00:07:04 +0800

    update

---
+++ src/main/java/com/example/capp/service/impl/ContentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.capp.service.impl;
+
+import com.example.capp.service.ContentService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContentServiceImpl implements ContentService {
+}
