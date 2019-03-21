908278e5c87568dc9f7825315bf76b6fea78131c
weimeng <1746387101@qq.com>
2019-03-21 18:51:16 +0800

    update

---
+++ src/main/java/com/example/capp/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.capp.controller;
+
+import com.example.capp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+}
